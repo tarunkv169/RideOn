512edb71bdfc3114161e41ec8c5caaecca530425
Tarunkumar-33 <tarunkv169@gamil.com>
2025-01-19 16:28:44 +0530

    Enhance user login and registration forms with state management and improved UI elements

--- frontend/src/pages/UserLogin.jsx
+++ frontend/src/pages/UserLogin.jsx
@@ -1,5 +1,5 @@
 // @ts-nocheck
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import { Link } from "react-router-dom";
 import rideon from "../assets/photos/rideon.png"
 import car from "../assets/photos/car.jpg"
@@ -11,7 +11,11 @@ const Userlogin = () => {
   const [password,setPassword]=useState('');
   const [userData,setUserData]=useState({});
 
-  const submitHandler=(e)=>{
+  useEffect(()=>{
+    console.log({userData});
+  },[userData])
+
+  const onSubmitHandler=async(e)=>{
 
     e.preventDefault();
     
@@ -20,7 +24,7 @@ const Userlogin = () => {
       password: password
     })
 
-    console.log(userData);
+    
 
     setEmail('')
     setPassword('')
@@ -29,28 +33,29 @@ const Userlogin = () => {
   return (
     <div className="min-h-screen flex items-center justify-center bg-center bg-cover md:bg-contain lg:bg-cover bg-[url(https://images.pexels.com/photos/5584203/pexels-photo-5584203.jpeg?auto=compress&cs=tinysrgb&w=600&lazy=load)]">
       <div className="py-12 px-4 w-full max-w-lg h-screen w-full">
-        <div className="h-full flex flex-col justify-between bg-white/30 backdrop-blur-md border-1 border-gray-700 rounded-xl">
+        <div className="h-full flex flex-col justify-between bg-white/26 backdrop-blur-lg border border-yellow-500 rounded-xl">
           <div>
             <img src={rideon} alt="no_img" className="w-28 mb-10 mt-4 mx-auto" />
-            <form onSubmit={(e) => submitHandler(e)}>
+            <form onSubmit={(e) => onSubmitHandler(e)}>
+
               <h3 className="text-lg font-medium mb-2 ml-10">What's your email</h3>
               <input
                 required
                 value={email}
                 onChange={(e) => {
-                    setEmail(e.target.value);
-                    }}
-                    type="email"
-                    placeholder="email@example.com"
-                    autoComplete="current-email"
-                    className="flex items-center justify-center mb-7 mx-auto py-2 px-4 text-lg w-10/12 bg-[#f2f2f2] rounded border placeholder:text-base"
-                    />
+                setEmail(e.target.value);
+                }}
+                type="email"
+                placeholder="email@example.com"
+                autoComplete="current-email"
+                className="flex items-center justify-center mb-7 mx-auto py-2 px-4 text-lg w-10/12 bg-[#f2f2f2] rounded border placeholder:text-base"
+              />
 
-                    <h3 className="text-lg font-medium mb-2 ml-10">Enter password</h3>
-                    <input
-                    required
-                    value={password}
-                    onChange={(e) => {
+              <h3 className="text-lg font-medium mb-2 ml-10">Enter password</h3>
+              <input
+                required
+                value={password}
+                onChange={(e) => {
                   setPassword(e.target.value);
                 }}
                 type="password"
@@ -59,19 +64,11 @@ const Userlogin = () => {
                 className="flex items-center justify-center mb-7 mx-auto py-2 px-4 text-lg w-10/12 bg-[#f2f2f2] rounded border placeholder:text-base"
               />
 
-              <button
-                type="submit"
-                className="inline-block flex items-center justify-center text-white text-lg font-semibold mx-auto py-2 px-4 w-64 bg-black rounded-md"
-              >
-                Log in
-              </button>
-              <p className="text-center mt-2">
-                New here?{" "}
-                <Link to={"/register"} className="text-blue-600">
-                  Create an account
-                </Link>
-              </p>
+              <button  className="inline-block flex items-center justify-center text-white text-lg font-semibold mx-auto py-2 px-4 w-64 bg-black rounded-md">Log in</button>
             </form>
+
+
+              <p className="text-center text-md mt-2">New here?{" "}<Link to={"/register"} className="text-[#3c95de] font-semibold">Create an account</Link></p>
           </div>
           <div>
             <Link
@@ -88,3 +85,9 @@ const Userlogin = () => {
 };
 
 export default Userlogin;
+
+
+
+
+
+
